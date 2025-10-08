5858a4add7ef2c9b9a08d98162edb1b35da3887b
mark3245 <mark3245@example.com>
2025-10-08 15:10:58 +0700

    chore: allow DB path override for Render ephemeral FS

--- altai_resort/settings.py
+++ altai_resort/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'altai_resort.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('SQLITE_PATH', default=str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
